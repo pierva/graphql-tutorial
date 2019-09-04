33b864aec7dc3107c3c7f53f028a36880d8dceab
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-09-03 21:39:52 -0400

    feat: Add update comment mutation operation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -162,6 +162,21 @@ const Mutation = {
         const deletedComment = db.comments.splice(commentIndex, 1)
 
         return deletedComment[0]
+    },
+
+    updateComment(parent, args, { db }, info) {
+        const { id, data } = args
+        const comment = db.comments.find((comment) => comment.id === id)
+
+        if(!comment) {
+            throw new Error('Comment not found')
+        }
+
+        if(typeof data.text === 'string') {
+            comment.text = data.text
+        }
+
+        return comment
     }
 }
 
