799e9fe87976e552fd2bc5f500e87f2a7ee3afdc
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-09-05 21:41:54 -0400

    chore: Setup Subscription boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
@@ -30,17 +31,21 @@ import Comment from './resolvers/Comment'
     only the db property off the context object.
 */
 
+const pubsub = new PubSub()
+
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         User,
         Post,
         Comment
     },
     context: {
-        db
+        db,
+        pubsub
     }
 })
 
