d287fd500a4065e292286209938162b8cb6631ef
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-09-13 21:59:56 -0400

    fix: Update comment subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -190,24 +190,35 @@ const Mutation = {
         }
 
         db.comments.push(comment)
-        pubsub.publish(`comment ${args.data.post}`, { comment })
+        pubsub.publish(`comment ${args.data.post}`, { 
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            } })
 
         return comment
     },
 
-    deleteComment(parent, args, { db }, info) {
+    deleteComment(parent, args, { db, pubsub }, info) {
         const commentIndex = db.comments.findIndex((comment) => comment.id === args.id)
 
         if(commentIndex === -1){
             throw new Error("Comment not found")
         }
 
-        const deletedComment = db.comments.splice(commentIndex, 1)
+        const [deletedComment] = db.comments.splice(commentIndex, 1)
 
-        return deletedComment[0]
+        pubsub.publish(`comment ${deleteComment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: deletedComment
+            }
+        })
+
+        return deletedComment
     },
 
-    updateComment(parent, args, { db }, info) {
+    updateComment(parent, args, { db, pubsub }, info) {
         const { id, data } = args
         const comment = db.comments.find((comment) => comment.id === id)
 
@@ -218,6 +229,13 @@ const Mutation = {
         if(typeof data.text === 'string') {
             comment.text = data.text
         }
+
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        })
 
         return comment
     }
